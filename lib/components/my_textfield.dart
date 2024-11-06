c01f2569842638b5558ad5fb5ad1933ce2259ef5
Ashwini Gawari <ashwinigawari27@gmail.com>
2024-11-06 22:47:52 +0530

    1st commit

---
+++ lib/components/my_textfield.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MyTextfield extends StatelessWidget {
+  final controller;
+  final String hintText;
+  final bool obscureText;
+
+  const MyTextfield({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Color.fromRGBO(189, 189, 189, 1)),
+            ),
+            fillColor: Color(0xFFCFC8C8),
+            filled: true,
+            hintText: hintText,
+            hintStyle: TextStyle(color: Colors.grey[500])
+
+            ),
+      ),
+    );
+  }
+}
